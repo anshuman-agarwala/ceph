2e47f37b332797742968ef15635aa0435fd1feba
Anshuman <anshuman@ibm.com>
2025-05-29 14:52:15 +0530

    changed compiler

--- src/script/run-make.sh
+++ src/script/run-make.sh
@@ -110,7 +110,7 @@ EOM
     local cxx_compiler="${discovered_cxx_compiler}"
     local c_compiler="${discovered_c_compiler}"
     local cmake_opts
-    cmake_opts+=" -DCMAKE_CXX_COMPILER=$cxx_compiler -DCMAKE_C_COMPILER=$c_compiler"
+    cmake_opts+=" -DCMAKE_CXX_COMPILER=/usr/lib64/ccache/clang++ -DCMAKE_C_COMPILER=/usr/lib64/ccache/clang"
     #cmake_opts+=" -DCMAKE_CXX_FLAGS_DEBUG=-Werror"
     cmake_opts+=" -DENABLE_GIT_VERSION=OFF"
     cmake_opts+=" -DWITH_GTEST_PARALLEL=ON"
